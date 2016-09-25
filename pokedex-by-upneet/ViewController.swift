d3be46bf320da4fe433839f1d4ad0d8c7d0ad24c
upneetrandhawa <upneet.randhawa@mail.utoronto.ca>
2016-09-25 04:23:36 -0400

    Initial Commit

---
+++ pokedex-by-upneet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-upneet
+//
+//  Created by Upneet  Randhawa on 2016-09-25.
+//  Copyright © 2016 Upneet  Randhawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
